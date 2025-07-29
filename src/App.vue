ca8e7f1c6c8082798b3e865944e1b73d3730e18b
Lucas Martim <l_martim@yahoo.com.br>
2025-07-28 22:54:22 -0300

    fix: enemies checkboxes, combatants index and enemies technique description

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="combat-config">
     <div class="combat-track">
-      <div>
+      <div class="boxes">
         <b>Combatants</b>
         <div class="turns-table">
-          <div class="defaul-table" v-if="!combatants?.length">
+          <div class="default-table" v-if="!combatants?.length">
             Vazio
           </div>
           <div class="turns-table__combatants" v-else>
@@ -13,40 +13,45 @@
                 <div>{{ slotProps.option.character ?? slotProps.option.enemy + ` #${slotProps.option.id}` }}</div>
                 <Select v-if="slotProps.option.player" v-model="playersApproaches[slotProps.option.player]" :options="approaches" optionLabel="approach" placeholder="Postura" size="small" />
                 <div v-if="slotProps.option.enemy" class="fatigues">
-                  <Checkbox v-for="fatigue, key in slotProps.option.fatigue" v-model="fatigues[key]" binary :key="key" />
+                  <Checkbox v-for="fatigue, key in slotProps.option.fatigue" binary :key="key" />
                 </div>
               </template>
             </Listbox>
           </div>
         </div>
       </div>
-      <div>
+      <div class="boxes">
         <b>Combat Track</b>
-        <div class="turns-table defaul-table">
-          <div v-if="!combatantsOrder?.length">
+        <div class="turns-table">
+          <div class="default-table" v-if="!combatantsOrder?.length">
             Vazio
           </div>
           <div class="turns-table__combatants" v-else>
-            <div class="turns-table__combatants__combatant" v-for="combatant, key in combatantsOrder" :key="key">
+            <Listbox v-model="selectedCombatantOrder" :options="combatantsOrder">
+              <template #option="slotProps">
+                <div>{{ slotProps.option.character ?? slotProps.option.enemy + ` #${slotProps.option.id}` }}</div>
+              </template>
+            </Listbox>
+            <!-- <div class="turns-table__combatants__combatant" v-for="combatant, key in combatantsOrder" :key="key" @click="selectCombatant(combatant)">
               <span>{{ combatant.character ?? combatant.enemy + ` #${combatant.id}`  }}</span>
               <span v-tooltip="combatant.enemy ? selectEnemyTechnique(combatant.selectedApproach) + ' - ' + selectEnemyTechniqueDescription(combatant.selectedApproach) : ''">
                 {{ combatant.selectedApproach.approach }} 
               </span>
-            </div>
+            </div> -->
           </div>
         </div>
       </div>
     </div>
     <div class="combat-config__buttons">
       <Button label="Ordernar" @click="orderCombatants" />
-      <Button label="Remover" severity="danger" @click="removerCombatant" />
+      <Button label="Remover" severity="danger" @click="removeCombatant" />
     </div>
     <div class="listboxes-list">
-      <div class="listbox-container">
+      <div class="listbox-container boxes">
         <b>Players</b>
         <Listbox v-model="selectedPlayers" :options="players" multiple optionLabel="character" class="listbox" />
       </div>
-      <div class="listbox-container">
+      <div class="listbox-container boxes">
         <b>Inimigos</b>
         <Listbox v-model="selectedEnemies" :options="enemiesUpdated" multiple class="listbox">
           <template #option="slotProps">
@@ -58,6 +63,12 @@
     <div class="combat-config__buttons">
       <Button label="Adicionar" @click="addToTrack" />
       <Button label="Resetar" severity="danger" @click="resetTrack" />
+    </div>
+    <div v-if="selectedCombatantOrder && selectedCombatantOrder.enemy" class="default-table">
+        <b>{{ selectedCombatantOrder.enemy }} #{{ selectedCombatantOrder.id }}</b>
+        <div><b>Abordagem/Movimento</b>: {{ selectedCombatantOrder.selectedApproach.approach }} / {{ selectEnemyTechnique(selectedCombatantOrder.selectedApproach) }}</div>
+        <div><b>Descrição</b>: {{ selectEnemyTechniqueDescription(selectedCombatantOrder.selectedApproach) }}</div>
+        <div><b>Alvos</b>: {{ selectEnemyTargets() }}</div>
     </div>
   </section>
 </template>
@@ -80,10 +91,14 @@
   let selectedPlayers = ref()
   let selectedEnemies = ref()
   let selectedCombatant = ref()
+  let selectedCombatantOrder = ref()
 
   let playersApproaches = ref([])
 
   let fatigues = ref([])
+
+  let playersIndex = ref(100)
+  let enemiesIndex = ref(0)
 
   const enemiesUpdated = computed(() => {
     const enemiesUpdated = []
@@ -114,29 +129,6 @@
       })
     })
 
-
-    // // Seleciona o primeiro approach aleatório
-    // const firstIndex = Math.floor(Math.random() * availableApproaches.length);
-    // const firstApproach = availableApproaches.splice(firstIndex, 1)[0];
-
-    // // Seleciona o segundo approach aleatório (garantido que será diferente)
-    // const secondIndex = Math.floor(Math.random() * availableApproaches.length);
-    // const secondApproach = availableApproaches.splice(secondIndex, 1)[0];
-
-    // // Atualiza técnicas avançadas se houver
-    // [firstApproach, secondApproach].forEach(approach => {
-    //   if (enemy.advancedTechniques?.length) {
-    //     advancedTechniques.forEach(advanced => {
-    //       if (advanced.id === approach.initiaive) {
-    //         approach.techniques.push({
-    //           technique: advanced.technique,
-    //           description: advanced.description,
-    //         });
-    //       }
-    //     });
-    //   }
-    // });
-
     return availableApproaches;
   };
 
@@ -144,11 +136,13 @@
     if (!selectedPlayers.value || !selectedEnemies.value) return;
 
     selectedPlayers.value.forEach((player, index) => {
-      combatants.value.push({ id: index + 90, index: index + 90, ...player })
+      playersIndex.value++
+      combatants.value.push({ id: playersIndex.value, index: playersIndex.value, ...player })
     })
 
     selectedEnemies.value.forEach((enemy, index) => {
-      combatants.value.push({ id: index + 1, index, ...enemy })
+      enemiesIndex.value++
+      combatants.value.push({ id: enemiesIndex.value, index: enemiesIndex.value, ...enemy })
     })
 
     selectedPlayers.value = []
@@ -162,29 +156,15 @@
     selectedPlayers.value = []
     selectedEnemies.value = []
     selectedCombatant.value = []
+    selectedCombatantOrder.value = []
+
+    playersIndex.value = 100
+    enemiesIndex.value = 0
   }
 
   const selectEnemyApproach = (enemyApproaches) => {
     const firstIndex = Math.floor(Math.random() * enemyApproaches.length);
     return enemyApproaches[firstIndex];
-
-    // // Seleciona o segundo approach aleatório (garantido que será diferente)
-    // const secondIndex = Math.floor(Math.random() * availableApproaches.length);
-    // const secondApproach = availableApproaches.splice(secondIndex, 1)[0];
-
-    // // Atualiza técnicas avançadas se houver
-    // [firstApproach, secondApproach].forEach(approach => {
-    //   if (enemy.advancedTechniques?.length) {
-    //     advancedTechniques.forEach(advanced => {
-    //       if (advanced.id === approach.initiaive) {
-    //         approach.techniques.push({
-    //           technique: advanced.technique,
-    //           description: advanced.description,
-    //         });
-    //       }
-    //     });
-    //   }
-    // });
   }
 
   const selectEnemyTechnique = (enemyApproach) => {
@@ -193,6 +173,16 @@
 
   const selectEnemyTechniqueDescription = (enemyApproach) => {
     return enemyApproach.techniques[Math.floor(Math.random() * enemyApproach.techniques.length)].description
+  }
+
+  const selectEnemyTargets = () => {
+    const firstTarget = players[Math.floor(Math.random() * players.length)]
+    let secondTarget = players[Math.floor(Math.random() * players.length)]
+
+    while (secondTarget == firstTarget)
+      secondTarget = players[Math.floor(Math.random() * players.length)]
+    
+    return `1º ${firstTarget.character} - 2º ${secondTarget.character}` 
   }
 
   const orderCombatants = () => {
@@ -222,7 +212,9 @@
     return combatant.character ?? combatant.enemy
   }
 
-  const removerCombatant = () => {
+  const removeCombatant = () => {
+    if (!selectedCombatant.value) return;
+    
     const combatantIndex = combatants.value.findIndex(combatant => combatant.index === selectedCombatant.value.index)
     combatants.value.splice(combatantIndex, 1)
 
@@ -235,10 +227,15 @@
   display: grid;
   row-gap: 16px;
 
-  .defaul-table {
+  .default-table {
     border: 1px solid #cbd5e1;
     border-radius: 4px;
     padding: 0.5rem 0.75rem;
+    width: 100%;
+    text-align: left;
+  }
+
+  .boxes {
     width: 100%;
   }
 
@@ -246,10 +243,11 @@
     display: flex;
     column-gap: 16px;
     text-align: left;
+    width: 100%;
 
     .turns-table {
       border-radius: 4px;
-      width: 350px;
+      width: 100%;
       display: flex;
       justify-content: flex-start;
       text-align: left;
@@ -291,7 +289,7 @@
       text-align: left;
       
       .listbox {
-        width: 350px;
+        width: 100%;
       }
     }
   }
